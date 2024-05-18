668c026a7ccc741b6d704d24ac7983ebdcbf36b4
Ibeaka Godson <godsonibeaka@yahoo.com>
2024-05-19 00:09:22 +0100

    updates

--- tublian_dashbord/src/App.jsx
+++ tublian_dashbord/src/App.jsx
@@ -1,12 +1,39 @@
-import React from "react";
-// import SideBar from "./components/SideBar";
-import ContentPage from "./components/ContentPage";
+import React, { useState } from "react";
+import search from "/icons/search-normal.png";
+import menuIcon from "/icons/menu.png";
 function App() {
+  const [menuBtn, setMenuBtn] = useState(false);
+  const 
+  function handleMenu() {
+    setMenuBtn((i) => !i);
+  }
   return (
     <>
       <div className="App flex">
-        {/* <SideBar /> */}
-        <ContentPage />
+        <aside>
+          <div className="flex items-center">
+            <button className="btn bg-neutral">
+              <img src={search} alt="" />
+            </button>
+            <button onClick={handleMenu}>
+              <img src={menuIcon} className="btn bg-neutral w-[4rem]" alt="" />
+            </button>
+          </div>
+          {menuBtn && (
+            <nav className="menu">
+              <ul>
+                <li> dashboard</li>
+                <li> monetiaztion</li>
+                <li> notification</li>
+              </ul>
+              <ul>
+                <li> logout</li>
+                <li> settings</li>
+              </ul>
+            </nav>
+          )}
+        </aside>
+
       </div>
     </>
   );
