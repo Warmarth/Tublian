aadb565734f5456be2b41e258b33b93119f6094f
Ibeaka Godson <godsonibeaka@yahoo.com>
2024-05-24 01:04:08 +0100

    update

--- tublian_dashbord/src/App.jsx
+++ tublian_dashbord/src/App.jsx
@@ -11,23 +11,28 @@ function App() {
   }
   return (
     <>
-      <div className="App w-[25rem] border overflow-hidden bg-[#1E1E1E] text-slate-100 ">
-        <aside className=" border">
-          <div className="border flex items-center flex-1  w-full  justify-end gap-2 ">
+      <div className="App w-[25rem] h-[100vh] border overflow-hidden bg-[#1E1E1E] text-slate-100  px-4">
+        <aside className="">
+          <div className=" flex items-center flex-1  w-full  justify-end gap-2 ">
             <input
               type="text"
-              placeholder="jejejjeje"
-              className="w-full pt-1 pb-1 rounded text-xl border-none outline-none bg-slate-600"
+              placeholder="search"
+              className="w-full pt-1 pb-1 pl-3 rounded text-xl border-none outline-none bg50"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
             />
-            <button className="btn bg-neutral">
+            <button className="px-4 py-2 rounded-md border-none outline-none  ">
               <img src={search} alt="search icon" />
             </button>
-            <button onClick={handleMenu} className="btn bg-neutral ">
+            <button
+              onClick={handleMenu}
+              className="px-4 py-2 rounded-md   border-none outline-none   "
+            >
               <img src={menuIcon} alt="menu icon" />
             </button>
           </div>
           {menuBtn && (
-            <nav className="menu space-y-2 transition-all">
+            <nav className="menu space-y-2 transition-all ">
               <ul>
                 <li> dashboard</li>
                 <li> monetiaztion</li>
@@ -43,52 +48,50 @@ function App() {
         <main>
           <header className="capitalize my-3">
             <h1 className="text-xl py-2">find Developers</h1>
-            <div className=" flex justify-between items-center border">
-              <div className="flex gap-2 bg-slate-600 text-slate-100 text-md border border-fuchsia-400 items-center p-[.2rem] rounded-md">
-                <p className="border   p-[.4rem] rounded-md"> full time</p>
+            <div className=" flex justify-between items-center ">
+              <div className="flex gap-2 bg50 text-p text-md  items-center p-[.2rem] rounded-md">
+                <p className="gradient p-[.4rem] rounded-md"> full time</p>
                 <p className="pr-[.3rem]"> freelance</p>
               </div>
-              <button className="btn btn-neutral ">
-                <img src={filter} alt="filter icon" />
+              <button className="px-6 py-2 rounded-md  bg50 border-none outline-none   ">
+                <img src={filter} alt="filter icon " className="m-auto" />
               </button>
             </div>
           </header>
-          <section className="flex gap-3 items-center justify-start no-wrap capitalize">
-            <span className="border btn btn-outline text-slate-100">
-              {" "}
-              discover
-            </span>
+
+          <section className="flex  gap-3 items-center justify-start no-wrap capitalize">
+            <p className="text-p axe"> discover</p>
             <span className="btn "> frontend developer</span>
             <span className="btn "> backend developer</span>
           </section>
-          <section className="mt-3">
-            <article className="flex flex-col">
-              <section className="flex items-start justify-between">
-                <div className="flex border gap-4">
+          <section className="mt-3  px-3 py-2 rounded-box bg50">
+            <article className="flex flex-col ">
+              <section className="flex items-start justify-between py-1">
+                <div className="flex gap-4">
                   <div>
                     <img src={frame} alt=" avater" />
                   </div>
-                  <div>
-                    <h3>john doe</h3>
-                    <div className=" ">
-                      <p> New jersey, USA</p>
-                      <p>$200,000[USA]</p>
-                    </div>
-                    <p>frontend Developer</p>
+                  <div className="font-mono pb-2 text-s pr-2">
+                    <h3 className="mb-1 text-xl text-p capitalize">john doe</h3>
+                    <p> New jersey,USA</p>
+                    <p>$200,000[USA]</p>
+                    <p className="border px-2 text-p rounded-xl">
+                      frontend developer
+                    </p>
                   </div>
                 </div>
-                <button>hire me</button>
+                <button className="b capitalize px-3 py-1 font-medium border-none text-[#1e1e1e] rounded-2xl ">
+                  hire me
+                </button>
               </section>
-              <section className="flex">
-                <div>
-                  <div>
-                    <p>other</p>
-                    <h2>porfolio</h2>
-                    <p>text Lorem ipsum dolor .</p>
-                  </div>
+              <section className=" flex gap-2 items-center justify-start font-mono p-1 text-sm on-wrap overflow-hidden">
+                <div className="border p-2  w-72 text-[.8rem]">
+                  <p className="border">other</p>
+                  <h2>porfolio</h2>
+                  <p>text Lorem ipsum dolor .</p>
                   <p>latest update: tue,dec 13, 2022</p>
                 </div>
-                <div>
+                <div className="border  w- text-[.8rem]">
                   <div>
                     <p>other</p>
                     <h2>porfolio</h2>
