ed4c45dacc7c932459be382ae9fe81ba32cb5646
Ibeaka Godson <godsonibeaka@yahoo.com>
2024-05-24 21:12:56 +0100

    update

--- tublian_dashbord/src/App.1.jsx
+++ tublian_dashbord/src/App.1.jsx
@@ -3,7 +3,8 @@ import search from "/icons/search-normal.png";
 import menuIcon from "/icons/menu.png";
 import filter from "/icons/filter.png";
 import frame from "/icons/Frame.png";
-function App() {
+
+export function App() {
   const [menuBtn, setMenuBtn] = useState(false);
   const [query, setQuery] = useState("");
   function handleMenu() {
@@ -53,7 +54,7 @@ function App() {
                 <p className="gradient p-[.4rem] rounded-md"> full time</p>
                 <p className="pr-[.3rem]"> freelance</p>
               </div>
-              <button className="px-6 py-2 rounded-md  bg50 border-none outline-none ">
+              <button className="px-6 py-2 rounded-md  bg50 border-none outline-none   ">
                 <img src={filter} alt="filter icon " className="m-auto" />
               </button>
             </div>
@@ -73,53 +74,48 @@ function App() {
           <section className="mt-3  px-3 pt-3 pb-5 rounded-box bg50 overflow-hidden">
             <article className=" ">
               {/* diffrent  */}
-              <AvaterCard />
+              <section className="flex items-start justify-between py-1">
+                <div className="flex gap-4">
+                  <div>
+                    <img src={frame} alt=" avater" />
+                  </div>
+                  <div className="font-mono pb-2 text-s pr-2">
+                    <h3 className="mb-1 text-xl text-p capitalize">john doe</h3>
+                    <p> New jersey,USA</p>
+                    <p>$200,000[USA]</p>
+                    <p className="border px-2 text-p rounded-xl">
+                      frontend developer
+                    </p>
+                  </div>
+                </div>
+                <button className="b capitalize px-3 py-1 font-medium border-none text-[#1e1e1e] rounded-2xl ">
+                  hire me
+                </button>
+              </section>
               <section className=" flex gap-2 items-center justify-start font-mono text-sm on-wrap overflow-hidden   border ">
-                <Card />
-                <Card />
+                <div className="border rounded-xl p-3  text-[.9rem] space-y-2 text-p">
+                  <span className="border rounded-xl px-4 py-1">Other</span>
+                  <h2 className="text-lg capitalize">porfolio</h2>
+                  <p className="text-s">
+                    text Lorem ipsum dolor Lorem ipsum dolor, sit amet
+                    consectetur adipisicing elit .
+                  </p>
+                  <p className="text-s">
+                    latest update:{" "}
+                    <span className="text-p"> tue,dec 13, 2022</span>{" "}
+                  </p>
+                </div>
+                {/* <div className="border rounded-xl p-3  w-72 text-[.7rem] ">
+              <p>other</p>
+              <h2>porfolio</h2>
+              <p>text Lorem ipsum dolor .</p>
+              <p>latest update: tue,dec 13, 2022</p>
+            </div> */}
               </section>
             </article>
           </section>
         </main>
       </div>
     </>
-  );
-}
-
-export default App;
-function Card() {
-  return (
-    <div className="border rounded-xl p-3  text-[.9rem] space-y-2 text-p">
-      <span className="border rounded-xl px-4 py-1">Other</span>
-      <h2 className="text-lg capitalize">porfolio</h2>
-      <p className="text-s">
-        text Lorem ipsum dolor Lorem ipsum dolor, sit amet consectetur
-        adipisicing elit .
-      </p>
-      <p className="text-s">
-        latest update: <span className="text-p"> tue,dec 13, 2022</span>{" "}
-      </p>
-    </div>
-  );
-}
-
-function AvaterCard() {
-  return (
-    <section className="flex items-start justify-between py-1">
-      <div className="flex gap-4">
-        <div>
-          <img src={frame} alt=" avater" />
-        </div>
-        <div className="font-mono pb-2 text-s pr-2">
-          <h3 className="mb-1 text-xl text-p capitalize">john doe</h3>
-          <p> New jersey,USA</p>
-          <p>$200,000[USA]</p>
-          <p className="border px-2 text-p rounded-xl">frontend developer</p>
-        </div>
-      </div>
-      <button className="b capitalize px-3 py-1 font-medium border-none text-[#1e1e1e] rounded-2xl ">
-        hire me
-      </button>
-    </section>
   );
 }
