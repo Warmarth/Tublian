085693211d88e29e9bc4efb479a5e22d22e8619e
Ibeaka Godson <godsonibeaka@yahoo.com>
2024-06-03 01:16:15 +0100

    update

--- ai_project/index.js
+++ ai_project/index.js
@@ -1,22 +1,48 @@
 const Groq = require("groq-sdk");
+const readLine = require("readline");
 require("dotenv").config();
 
 const groq = new Groq((api_key = process.env.GROQ_API_KEY));
 
-async function main() {
-  const completion = await groq.chat.completions
+const rl = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("ask Your question ? ", (question) => {
+  groq.chat.completions
     .create({
       messages: [
         {
           role: "user",
-          content: "what are the causes of climate change",
+          content: question,
         },
       ],
       model: "llama3-8b-8192",
     })
     .then((chatCompletion) => {
-      process.stdout.write(chatCompletion.choices[0]?.message?.content || "");
+      rl.output.write(chatCompletion.choices[0]?.message?.content || "");
     });
-}
 
-main();
+    
+
+  rl.close();
+});
+
+// async function main() {
+//   const completion = await groq.chat.completions
+//     .create({
+//       messages: [
+//         {
+//           role: "user",
+//           content: "what are the causes of climate change",
+//         },
+//       ],
+//       model: "llama3-8b-8192",
+//     })
+//     .then((chatCompletion) => {
+//       process.stdout.write(chatCompletion.choices[0]?.message?.content || "");
+//     });
+// }
+
+// main();
